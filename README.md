d8841f8afdc97280ea0a4e2ba49763fd91f3add0
Yoni Water Man <yonixw@gmail.com>
2024-11-04 21:06:05 +0000

    config working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # notepadWW
 Trying to do a web based notepad++, but don't hold your breath
+
+# Creation
+Created using: https://ui.shadcn.com/docs/installation
--- notepadWW/src/App.tsx
+++ notepadWW/src/App.tsx
@@ -7,36 +7,35 @@ import { Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
 
+import Editor from '@monaco-editor/react';
+
+
 function App() {
-  const [count, setCount] = useState(0)
   const [isLoad,setIsLoad] = useState(false);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Button variant={"outline"} disabled={isLoad} onClick={()=>setIsLoad(true)} >
+      <Button disabled={isLoad} onClick={()=>{setIsLoad(true); setTimeout(()=>setIsLoad(false),3000)}} >
           {isLoad &&  <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
           Click Shad
           </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <br />
+      <div>
+        {/*
+        https://microsoft.github.io/monaco-editor/typedoc/interfaces/editor.IStandaloneEditorConstructionOptions.html
+        https://www.npmjs.com/package/@monaco-editor/react
+        */}
+        {/*
+        https://ui.shadcn.com/docs/components/menubar
+        https://lucide.dev/icons/
+        */}
+      <Editor height="90vh" width="50vw" theme="vs-dark" 
+        options={{renderWhitespace: "all", fontSize:18, wordWrap:"on", }} 
+        defaultLanguage="javascript" defaultValue="// some comment" />
+      </div>
+      
     </>
   )
 }
